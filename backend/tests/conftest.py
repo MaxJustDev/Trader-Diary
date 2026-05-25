af14923e52d67fe23835f18972a8a44c950c455d
MaxJustDev <ngonguyenducmanh@gmail.com>
2026-05-25 07:37:51 +0700

    test: add pytest scaffolding for backend

---
+++ backend/tests/conftest.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Make `app` importable when running pytest from backend/
+_BACKEND_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if _BACKEND_DIR not in sys.path:
+    sys.path.insert(0, _BACKEND_DIR)
